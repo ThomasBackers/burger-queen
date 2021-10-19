92ed2a041696193a1856cbb11aaa854e34f6328c
ThomasBackers <thomasbackers@gmail.com>
2021-10-19 18:03:47 +0200

    minor fix

--- public/js/side-menu.js
+++ public/js/side-menu.js
@@ -1,8 +1,9 @@
+const sideMenu = document.querySelector(".header__side-menu")
 // SHOW SIDE MENU
 document.querySelector(".fa-bars").addEventListener("click", () => {
-    document.querySelector(".header__side-menu").style.transform = "translateX(100%)";
-});
+    sideMenu.style.transform = "translateX(100%)"
+})
 // HIDE SIDE MENU
 document.querySelector(".header__side-menu__side-void").addEventListener("click", () => {
-    document.querySelector(".header__side-menu").style.transform = "";
-});
+    sideMenu.style.transform = ""
+})
